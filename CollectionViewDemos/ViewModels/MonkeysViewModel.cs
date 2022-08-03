edd90c36601d0e1ff998393fe27f3dacb4e71826
Blago <blago.culjak@hotmail.com>
2022-08-03 15:14:27 +0200

    Add project files.

---
+++ CollectionViewDemos/ViewModels/MonkeysViewModel.cs
@@ -0,0 +1,174 @@
+﻿using CollectionViewDemos.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace CollectionViewDemos.ViewModels
+{
+    public class MonkeysViewModel : INotifyPropertyChanged
+    {
+        readonly IList<Monkey> source;
+        Monkey selectedMonkey;
+        int selectionCount = 1;
+
+        public ObservableCollection<Monkey> Monkeys { get; private set; }
+        public IList<Monkey> EmptyMonkeys { get; private set; }
+
+        public Monkey SelectedMonkey
+        {
+            get
+            {
+                return selectedMonkey;
+            }
+            set
+            {
+                if (selectedMonkey != value)
+                {
+                    selectedMonkey = value;
+                }
+            }
+        }
+
+        ObservableCollection<object> selectedMonkeys;
+        public ObservableCollection<object> SelectedMonkeys
+        {
+            get
+            {
+                return selectedMonkeys;
+            }
+            set
+            {
+                if (selectedMonkeys != value)
+                {
+                    selectedMonkeys = value;
+                }
+            }
+        }
+
+        public string SelectedMonkeyMessage { get; private set; }
+
+        public ICommand DeleteCommand => new Command<Monkey>(RemoveMonkey);
+        public ICommand FavoriteCommand => new Command<Monkey>(FavoriteMonkey);
+        public ICommand FilterCommand => new Command<string>(FilterItems);
+        public ICommand MonkeySelectionChangedCommand => new Command(MonkeySelectionChanged);
+
+        public MonkeysViewModel()
+        {
+            source = new List<Monkey>();
+            CreateMonkeyCollection();
+
+            selectedMonkey = Monkeys.Skip(3).FirstOrDefault();
+            MonkeySelectionChanged();
+
+            SelectedMonkeys = new ObservableCollection<object>()
+            {
+                Monkeys[1], Monkeys[3], Monkeys[4]
+            };
+        }
+
+        void CreateMonkeyCollection()
+        {
+            source.Add(new Monkey
+            {
+                Name = "Baboon",
+                Location = "Africa & Asia",
+                Details = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+            });
+
+            source.Add(new Monkey
+            {
+                Name = "Capuchin Monkey",
+                Location = "Central & South America",
+                Details = "The capuchin monkeys are New World monkeys of the subfamily Cebinae. Prior to 2011, the subfamily contained only a single genus, Cebus.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+            });
+
+            source.Add(new Monkey
+            {
+                Name = "Blue Monkey",
+                Location = "Central and East Africa",
+                Details = "The blue monkey or diademed monkey is a species of Old World monkey native to Central and East Africa, ranging from the upper Congo River basin east to the East African Rift and south to northern Angola and Zambia",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+            });
+
+            source.Add(new Monkey
+            {
+                Name = "Squirrel Monkey",
+                Location = "Central & South America",
+                Details = "The squirrel monkeys are the New World monkeys of the genus Saimiri. They are the only genus in the subfamily Saimirinae. The name of the genus Saimiri is of Tupi origin, and was also used as an English name by early researchers.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg/220px-Saimiri_sciureus-1_Luc_Viatour.jpg"
+            });
+
+            source.Add(new Monkey
+            {
+                Name = "Golden Lion Tamarin",
+                Location = "Brazil",
+                Details = "The golden lion tamarin also known as the golden marmoset, is a small New World monkey of the family Callitrichidae.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Golden_lion_tamarin_portrait3.jpg/220px-Golden_lion_tamarin_portrait3.jpg"
+            });
+
+            source.Add(new Monkey
+            {
+                Name = "Howler Monkey",
+                Location = "South America",
+                Details = "Howler monkeys are among the largest of the New World monkeys. Fifteen species are currently recognised. Previously classified in the family Cebidae, they are now placed in the family Atelidae.",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Alouatta_guariba.jpg/200px-Alouatta_guariba.jpg"
+            });
+
+           
+
+
+            Monkeys = new ObservableCollection<Monkey>(source);
+        }
+
+        void FilterItems(string filter)
+        {
+            var filteredItems = source.Where(monkey => monkey.Name.ToLower().Contains(filter.ToLower())).ToList();
+            foreach (var monkey in source)
+            {
+                if (!filteredItems.Contains(monkey))
+                {
+                    Monkeys.Remove(monkey);
+                }
+                else
+                {
+                    if (!Monkeys.Contains(monkey))
+                    {
+                        Monkeys.Add(monkey);
+                    }
+                }
+            }
+        }
+
+        void MonkeySelectionChanged()
+        {
+            SelectedMonkeyMessage = $"Selection {selectionCount}: {SelectedMonkey.Name}";
+            OnPropertyChanged("SelectedMonkeyMessage");
+            selectionCount++;
+        }
+
+        void RemoveMonkey(Monkey monkey)
+        {
+            if (Monkeys.Contains(monkey))
+            {
+                Monkeys.Remove(monkey);
+            }
+        }
+
+        void FavoriteMonkey(Monkey monkey)
+        {
+            monkey.IsFavorite = !monkey.IsFavorite;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
